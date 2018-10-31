9de735a33dffc76dd98f41f37f83f870172a80ef
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2018-10-31 11:08:39 +0530

    Initial Commit

---
+++ models/schema.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE IF NOT EXISTS DBMS_PROJECT;
+USE DBMS_PROJECT;
+
+-- Create a new table called 'Institute' in schema 'DBMS_PROJECT'
+CREATE TABLE IF NOT EXISTS DBMS_PROJECT.Institute
+(
+    InstituteId INT NOT NULL PRIMARY KEY, -- primary key column
+    Name VARCHAR(255) NOT NULL UNIQUE,
+    EstablishedYear INT NOT NULL
+);
+
+
+-- Create a new table called 'Student' in schema 'DBMS_PROJECT'
+CREATE TABLE  IF NOT EXISTS DBMS_PROJECT.Student
+(
+    RollNumber INT NOT NULL PRIMARY KEY, -- primary key column
+    Name VARCHAR(255) NOT NULL,
+    Rank INT NOT NULL UNIQUE,
+    Marks DECIMAL(5,2) NOT NULL
+);
+
+
+-- Create a new table called 'Program' in schema 'DBMS_PROJECT'
+CREATE TABLE IF NOT EXISTS DBMS_PROJECT.Program
+(
+    Name VARCHAR(255) NOT NULL,
+    TotalSeats INT NOT NULL,
+    SeatsLeft INT NOT NULL,
+    InstituteId INT NOT NULL,
+    FOREIGN KEY (InstituteId) REFERENCES DBMS_PROJECT.Institute(InstituteId),
+    PRIMARY KEY (InstituteId, Name)
+);
+
+
+-- Create a new table called 'ProgramChosen' in schema 'DBMS_PROJECT'
+CREATE TABLE  IF NOT EXISTS DBMS_PROJECT.ProgramChosen
+(
+  StudentRollNumber INT NOT NULL,
+  FOREIGN KEY (StudentRollNumber) REFERENCES DBMS_PROJECT.Student(RollNumber),
+  InstituteId INT NOT NULL,
+  FOREIGN KEY (InstituteId) REFERENCES DBMS_PROJECT.Program(InstituteId),
+  ProgramName VARCHAR(255) NOT NULL,
+  FOREIGN KEY (ProgramName) REFERENCES DBMS_PROJECT.Program(Name),
+  Priority INT NOT NULL,
+  PRIMARY KEY (StudentRollNumber, InstituteId, ProgramName, Priority)
+);
